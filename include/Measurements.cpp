9925d6c7da62752648fb7786c2c7913bd326d024
Andrew Frye <dewdwe72805@gmail.com>
2021-06-16 14:57:48 -0400

    Added more commands and started multithreading

--- include/Measurements.cpp
+++ include/Measurements.cpp
@@ -2,25 +2,70 @@
 
 using namespace vex;
 
-class measurements{
-  public:
-    int claw = 180;
-    int clawRing = 90;
+// Measurements used in the measured moves class
+class measurements {
+public:
+  int claw = 180;
+  int clawRing = 90;
 };
 
-class measuredMoves{
+// Measured Movements for Clawbot and Tipping Point Game
+class measuredMoves {
   measurements obj;
 
-  public:
-    void close(){
-      Claw.spinFor(forward, obj.claw, degrees);
-    }
+public:
+  // Close the claw From the open position
+  void close() { Claw.spinFor(forward, obj.claw, degrees); }
 
-    void open(){
-      Claw.spinFor(reverse, obj.claw, degrees);
-    }
+  // Open the claw from the closed position
+  int open() {
+    Claw.spinFor(reverse, obj.claw, degrees);
+    return 0;
+  }
 
-    void closeOnRing(){
-      Claw.spinFor(forward, obj.clawRing, degrees);
-    }
+  // Closes the claw from open onto a ring
+  void closeOnRing() { Claw.spinFor(forward, obj.clawRing, degrees); }
 };
+
+// Simple movement commands to streamline development
+class moveFunctions {
+
+private:
+  drivetrain train = Drivetrain;
+
+  
+public:
+  moveFunctions(drivetrain dtrain) { train = dtrain; }
+
+  // Drives in a given direction until a condition is met
+  void driveUntil(bool x, bool target, directionType dir) {
+    train.drive(dir);
+    waitUntil(x == target);
+    train.stop();
+  }
+
+  // Float override of previous function
+  void driveUntil(float x, float target, directionType dir) {
+    train.drive(dir);
+    waitUntil(x == target);
+    train.stop();
+  }
+
+  // Integer override of previous function
+  void driveUntil(int x, int target, directionType dir) {
+    train.drive(dir);
+    waitUntil(x == target);
+    train.stop();
+  }
+
+  // Double override of previous function
+  void driveUntil(double x, double target, directionType dir) {
+    train.drive(dir);
+    waitUntil(x == target);
+    train.stop();
+  }
+
+  
+};
+int callback(){return 0;}
+thread (callback);
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
-/*    Author:       Andrew Frye                                           */
-/*    Created:      Tuesday June 8, 2021                                           */
+/*    Author:       Andrew Frye                                               */
+/*    Created:      Tuesday June 8, 2021                                      */
 /*    Description:  V5 project                                                */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
@@ -10,35 +10,34 @@
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
 // [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// Drivetrain           drivetrain    1, 10           
-// Arm                  motor         2               
-// Claw                 motor         3               
-// Forks                motor_group   4, 5            
+// Controller1          controller
+// Drivetrain           drivetrain    1, 10
+// Arm                  motor         8
+// Claw                 motor         3
+// Forks                motor_group   4, 5
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
-#include "vex.h"
 #include "Measurements.cpp"
+#include "vex.h"
 
 using namespace vex;
 
-void roboConfigs(){
+//robot motor and sensor settings
+void roboConfigs() {
   Arm.setStopping(brake);
   Claw.setStopping(brake);
   Drivetrain.setStopping(brake);
   Forks.setStopping(brake);
-  Drivetrain.setDriveVelocity(100,percent);
+  Drivetrain.setDriveVelocity(100, percent);
 }
 
 int main() {
+
+  // initialize the robot
   vexcodeInit();
   roboConfigs();
 
-  measuredMoves obj;
-
-  obj.open();
-  wait(10, seconds);
-  obj.closeOnRing();
+  // Define classes to be used
+  measuredMoves calcMoves;
+  moveFunctions simpMoves(Drivetrain);
 }
-
-
