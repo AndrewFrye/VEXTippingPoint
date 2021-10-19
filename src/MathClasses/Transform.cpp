a535b8e07c8290f3b7522c4524bb1438f4d82a1f
Andrew Frye <dewdwe72805@gmail.com>
2021-10-18 21:52:28 -0400

    pushing previous changes

---
+++ src/MathClasses/Transform.cpp
@@ -0,0 +1,1 @@
+#include <MathClasses/Transform.h>
--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,7 @@
 // Forks                motor_group   4, 5
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
-#include "Measurements.cpp"
 #include "vex.h"
-#include <thread>
 
 using namespace vex;
 
@@ -32,26 +30,7 @@ void roboConfigs() {
   Drivetrain.setDriveVelocity(100, percent);
 }
 
-void moveThreadFactory(void *moveCmd) {
-  measuredMoves calcMoves;
 
-  int *intPtr{static_cast<int *>(moveCmd)};
-
-  switch ((int)*intPtr) {
-  case 1:
-    calcMoves.open();
-    break;
-  case 2:
-    calcMoves.close();
-    break;
-  case 3:
-    Arm.spinFor(reverse, 360, degrees);
-    break;
-  case 4:
-    Arm.spinFor(forward, 360, degrees);
-    break;
-  }
-}
 
 int main() {
 
@@ -59,32 +38,5 @@ int main() {
   vexcodeInit();
   roboConfigs();
 
-  // Define classes to be used
-  measuredMoves calcMoves;
-  moveFunctions simpMoves(Drivetrain);
-
-  int x = 1;
-  void *a = &x;
-  while(1)
-  {vex::thread t1(moveThreadFactory, a);
   
-  wait(500, msec);
-  x=3;
-  vex::thread t3(moveThreadFactory, a);
-  t3.detach();
-
-  t3.join();
-  t1.join();
-
-  x = 2;
-  vex::thread t2(moveThreadFactory, a);
-  
-  x=4;
-  vex::thread t4(moveThreadFactory, a);
-  t4.join();
-  t2.join();
-
-  Drivetrain.driveFor(forward, 200, mm);
-  Drivetrain.turnFor(left, 90, degrees);
-  }
 }
